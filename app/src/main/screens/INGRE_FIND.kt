29d45945d745c4b1feb86c2c87a51374081b54cb
rudghks01 <scvluncher@gmail.com>
2024-05-25 20:59:49 +0900

    ingre 팝업쪽 준비중

---
+++ app/src/main/screens/INGRE_FIND.kt
@@ -0,0 +1,25 @@
+package com.your.myapplication
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+class Frame84 : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_frame84)
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rtbr4frgwdth))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r04n036gcjgop))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rnmwu5cyoq8))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rj4k0fxg7iv))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r8x429eu816t))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rufvlaitr3v8))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rfuhzv3qv8bg))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r43akzm4jqsm))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rqk09h5i2hk))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r41rr8nwfbev))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rakkkobzxtag))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r9pyquhfyu))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r19h05ypeb91g))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rxr0aahod3k))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rmc1x1fi9e))
+	}
+}
---
+++ app/src/main/screens/INGRE_FINDPopUp.kt
@@ -0,0 +1,24 @@
+package com.your.myapplication
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+class INGRE_FINDPopUp : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_i_n_g_r_e__f_i_n_d_pop_up)
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r2xjrizjubu5))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rpqjhcfrnbqm))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.ro86enhzmrb))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.raodmkntqa3d))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rfe32kfgd9oq))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rb8u7fpp5x15))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r2h11e79bkwq))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r2w976pjv3m1))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rk23g1oyutjq))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rh2rjsskwhb7))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r6gnlg6m5nir))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rsirxs4w8lod))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rr8i9jb3zdx))
+		Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rl713sevsay))
+	}
+}
